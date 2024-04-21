43bf7d4061ec58f0024807e12e4724abbae525d6
OulipianSummer <benbow.drew@gmail.com>
2024-04-21 13:26:29 -0400

    Create some local development tools

---
+++ default.nix
@@ -0,0 +1,17 @@
+with (import <nixpkgs> {});
+let
+  gems = bundlerEnv {
+    name = "personal-website-bundler-env";
+    inherit ruby;
+    gemfile = ./Gemfile;
+    lockfile = ./local.Gemfile.lock;
+    gemset = ./gemset.nix;
+  };
+in stdenv.mkDerivation {
+  name = "personal-website";
+  buildInputs = [gems bundler ruby];
+
+  shellHook = ''
+    exec ${gems}/bin/jekyll serve --watch
+  '';
+}
